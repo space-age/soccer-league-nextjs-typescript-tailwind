d00daaf33bd456edb00eb8dad1a497340b52fe89
space-age <53498728+space-age@users.noreply.github.com>
2022-06-29 19:09:04 -0700

    commented out unuse component

--- components/masterCreator/commonComponents/seasonsData/SeasonTableRow copy.tsx
+++ components/masterCreator/commonComponents/seasonsData/SeasonTableRow copy.tsx
@@ -1,69 +1,81 @@
-import { DocumentData } from 'firebase/firestore'
-import { Team, TeamList } from '../../../../typings'
+/*
+  Component that does the calculations for the table row inside the component, instead of passing in the data as props
+*/
 
-interface Props {
-  team: TeamList | DocumentData
-  index: number
+import React from 'react'
+
+function SeasonTableRowCopy() {
+  return <div>-</div>
 }
 
-function SeasonTableRow({ team, index }: Props) {
-  const calculations = () => {
-    const defaultCalculations = {
-      counterW: 0,
-      counterPoints: 0,
-      counterD: 0,
-      counterL: 0,
-      counterGoalsScored: 0,
-      counterGoalsAgainst: 0,
-      counterGoalsDifference: 0,
-    }
-    if (!team.gamesPlayed) return defaultCalculations
-    else {
-      team.gamesPlayed.map((game) => {
-        if (game.result === 'W') {
-          defaultCalculations.counterW += 1
-          defaultCalculations.counterPoints += 3
-        }
-        if (game.result === 'D') {
-          defaultCalculations.counterD += 1
-          defaultCalculations.counterPoints += 1
-        }
-        if (game.result === 'D') {
-          defaultCalculations.counterL += 1
-        }
+export default SeasonTableRowCopy
 
-        defaultCalculations.counterGoalsScored += game.goalsScored
-        defaultCalculations.counterGoalsAgainst += game.goalsAgainst
-      })
-      defaultCalculations.counterGoalsDifference =
-        defaultCalculations.counterGoalsScored -
-        defaultCalculations.counterGoalsAgainst
-      return defaultCalculations
-    }
-  }
+// import { DocumentData } from 'firebase/firestore'
+// import { Team, TeamList } from '../../../../typings'
 
-  const teamCalc = calculations()
+// interface Props {
+//   team: TeamList | DocumentData
+//   index: number
+// }
 
-  return (
-    <tr>
-      <td>{++index}</td>
-      <td className="!text-left text-[#263238] lg:!pr-32 lg:!pl-12">
-        {team.name}
-      </td>
-      <td>{!team.gamesPlayed ? 0 : team.gamesPlayed.length}</td>
-      <td>{teamCalc.counterW}</td>
-      <td>{teamCalc.counterD}</td>
-      <td>{teamCalc.counterL}</td>
-      <td>{teamCalc.counterGoalsScored}</td>
-      <td>{teamCalc.counterGoalsAgainst}</td>
-      <td>
-        {teamCalc.counterGoalsDifference > 0
-          ? `+${teamCalc.counterGoalsDifference}`
-          : `${teamCalc.counterGoalsDifference}`}
-      </td>
-      <td className="font-black">{teamCalc.counterPoints}</td>
-    </tr>
-  )
-}
+// function SeasonTableRow({ team, index }: Props) {
+//   const calculations = () => {
+//     const defaultCalculations = {
+//       counterW: 0,
+//       counterPoints: 0,
+//       counterD: 0,
+//       counterL: 0,
+//       counterGoalsScored: 0,
+//       counterGoalsAgainst: 0,
+//       counterGoalsDifference: 0,
+//     }
+//     if (!team.gamesPlayed) return defaultCalculations
+//     else {
+//       team.gamesPlayed.map((game) => {
+//         if (game.result === 'W') {
+//           defaultCalculations.counterW += 1
+//           defaultCalculations.counterPoints += 3
+//         }
+//         if (game.result === 'D') {
+//           defaultCalculations.counterD += 1
+//           defaultCalculations.counterPoints += 1
+//         }
+//         if (game.result === 'D') {
+//           defaultCalculations.counterL += 1
+//         }
 
-export default SeasonTableRow
+//         defaultCalculations.counterGoalsScored += game.goalsScored
+//         defaultCalculations.counterGoalsAgainst += game.goalsAgainst
+//       })
+//       defaultCalculations.counterGoalsDifference =
+//         defaultCalculations.counterGoalsScored -
+//         defaultCalculations.counterGoalsAgainst
+//       return defaultCalculations
+//     }
+//   }
+
+//   const teamCalc = calculations()
+
+//   return (
+//     <tr>
+//       <td>{++index}</td>
+//       <td className="!text-left text-[#263238] lg:!pr-32 lg:!pl-12">
+//         {team.name}
+//       </td>
+//       <td>{!team.gamesPlayed ? 0 : team.gamesPlayed.length}</td>
+//       <td>{teamCalc.counterW}</td>
+//       <td>{teamCalc.counterD}</td>
+//       <td>{teamCalc.counterL}</td>
+//       <td>{teamCalc.counterGoalsScored}</td>
+//       <td>{teamCalc.counterGoalsAgainst}</td>
+//       <td>
+//         {teamCalc.counterGoalsDifference > 0
+//           ? `+${teamCalc.counterGoalsDifference}`
+//           : `${teamCalc.counterGoalsDifference}`}
+//       </td>
+//       <td className="font-black">{teamCalc.counterPoints}</td>
+//     </tr>
+//   )
+// }
+
+// export default SeasonTableRow
--- pages/table.tsx
+++ pages/table.tsx
@@ -5,6 +5,9 @@ import CommonBanner from '../components/commonComponents/CommonBanner'
 
 import image from '../images/table-bg.jpg'
 import WeekScheduleDropdown from '../components/schedulePage/lists/SchedulesSeasonsDropdown'
+import SchedulesSeasonsDropdown from '../components/schedulePage/lists/SchedulesSeasonsDropdown'
+import SchedulesDivisionsDropdown from '../components/schedulePage/lists/SchedulesDivisionsDropdown'
+import SchedulesWeekScheduleDropdown from '../components/schedulePage/lists/SchedulesWeekScheduleDropdown'
 
 function TablePage() {
   return (
@@ -16,7 +19,11 @@ function TablePage() {
       <Header />
       <main className="relative pb-4 md:pb-10">
         <CommonBanner image={image} title="Tables" />
-        <WeekScheduleDropdown />
+        <div className="mt-4 flex flex-col sm:mt-8 sm:flex-row sm:justify-center sm:gap-10">
+          <SchedulesSeasonsDropdown />
+          <SchedulesDivisionsDropdown />
+          <SchedulesWeekScheduleDropdown />
+        </div>
         <Tables />
       </main>
     </div>
